fa94694d87cb2e316c201b6f234bfb7376765215
Simon Marchi <simon.marchi@ericsson.com>
2016-08-19 10:28:47 -0400

    Set PATH to find compiler in polarsys-build-kernel.sh

--- polarsys-build-kernel.sh
+++ polarsys-build-kernel.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-MAKE="make -C linux ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf-"
+polarsys_basepath=$(readlink -f "$BASH_SOURCE[0]")
+polarsys_basepath=$(dirname $polarsys_basepath)
+PATH="$polarsys_basepath/raspberrypi-tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin/:$PATH"
+
+MAKE="make V=1 -C linux ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf-"
+
+echo "Using compiler $(which arm-linux-gnueabihf-gcc)"
 
 # linux-defconfig was generated using the kernel's "make savedefconfig"
 cp linux-defconfig linux/.config
