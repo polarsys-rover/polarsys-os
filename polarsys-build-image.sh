3e056b3f47c27a0b7c12713b292f8f40f5fd0f87
Simon Marchi <simon.marchi@ericsson.com>
2016-09-06 16:31:08 -0400

    Add pkg-config to the list of installed packages

--- polarsys-build-image.sh
+++ polarsys-build-image.sh
@@ -5,7 +5,11 @@ cd rpi23-gen-image
 PROXY=http://127.0.0.1:3142
 
 RPI_MODEL=2 \
-APT_INCLUDES="libmosquittopp-dev build-essential" \
+APT_INCLUDES=" \
+    build-essential \
+    libmosquittopp-dev \
+    pkg-config \
+    " \
 APT_PROXY=$PROXY \
 BUILD_KERNEL=true \
 KERNELSRC_DIR=../linux \
